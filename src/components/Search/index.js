d420e8b3e57a706d7ff9195bbf22e90d68e08cd7
Annie Kwan <akwan.mtl@gmail.com>
2021-02-24 17:44:24 -0500

    added a search input field component

---
+++ src/components/Search/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+
+function Search () {
+  return (
+    <div className="search-bar">
+      <input placeholder="Search"></input>
+    </div>
+  )
+}
+
+export default Search;
