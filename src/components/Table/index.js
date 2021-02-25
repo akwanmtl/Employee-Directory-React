ef01caf0c73687b38d1ea1fe3579e06800ccdc47
Annie Kwan <akwan.mtl@gmail.com>
2021-02-24 19:22:43 -0500

    added table component

---
+++ src/components/Table/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.css";
+
+
+function Table () {
+  return (
+    <div className="table-employee">
+      <table className="table table-hover">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Phone</th>
+          <th scope="col">Email</th>
+          <th scope="col">DOB</th>
+        </tr>
+
+      </table>
+    </div>
+  )
+}
+
+export default Table;
