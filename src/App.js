bb168aac5918d339018678562927ce8dd228b267
Annie Kwan <akwan.mtl@gmail.com>
2021-02-26 12:24:32 -0500

    fix state undefined
    added unique key
    
    tidied handleclick function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Main from "./components/Main";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
--- src/components/Main/Table.js
+++ src/components/Main/Table.js
@@ -10,15 +10,15 @@ function Table (props) {
         <thead>
           <tr>
             <th scope="col"></th>
-            <th scope="col" id="name" onClick={props.handleClick} className="pointer" >Name  <span className="icon"><FontAwesomeIcon icon={faCaretUp} className={`rotate-${props.toggle['name']}`}/></span></th>
-            <th scope="col" id="phone" onClick={props.handleClick} className="pointer">Phone  <span className="icon"><FontAwesomeIcon icon={faCaretUp} className={`rotate-${props.toggle['phone']}`}/></span></th>
-            <th scope="col" id="email" onClick={props.handleClick} className="pointer">Email  <span className="icon"><FontAwesomeIcon icon={faCaretUp} className={`rotate-${props.toggle['email']}`}/></span></th>
-            <th scope="col" id="dob" onClick={props.handleClick} className="pointer">DOB  <span className="icon"><FontAwesomeIcon icon={faCaretUp} className={`rotate-${props.toggle['dob']}`}/></span></th>
+            <th scope="col" onClick={() => props.handleClick('name')} className="pointer" >Name  <span className="icon"><FontAwesomeIcon icon={faCaretUp} className={`rotate-${props.toggle['name']}`}/></span></th>
+            <th scope="col" onClick={() => props.handleClick('phone')} className="pointer">Phone  <span className="icon"><FontAwesomeIcon icon={faCaretUp} className={`rotate-${props.toggle['phone']}`}/></span></th>
+            <th scope="col" onClick={() => props.handleClick('email')} className="pointer">Email  <span className="icon"><FontAwesomeIcon icon={faCaretUp} className={`rotate-${props.toggle['email']}`}/></span></th>
+            <th scope="col" onClick={() => props.handleClick('dob')} className="pointer">DOB  <span className="icon"><FontAwesomeIcon icon={faCaretUp} className={`rotate-${props.toggle['dob']}`}/></span></th>
           </tr>
         </thead>
         <tbody>
         {props.results.map(employee => (
-          <tr>
+          <tr key={employee.id}>
             <td><img src={employee.picture} alt={employee.name}/></td>
             <td>{employee.name}</td>
             <td>{employee.phone}</td>
